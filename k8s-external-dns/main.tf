69343b70a1ce724c2c626cb660c8a7f55e1643b8
Will <will@containerlabs.io>
2022-06-08 13:05:38 -0700

    creating one external-dns per hosted zone

--- k8s-external-dns/main.tf
+++ k8s-external-dns/main.tf
@@ -1,19 +1,24 @@
 locals {
   # k8s this should be the default calculated name anyway, but we want to enforce it just to be sure
   service_account_name = var.release == "external-dns" ? "external-dns" : "${var.release}-external-dns"
+  split_domain_filters = split(",", var.domain_filters)
+  release_name_map = {
+    for domain_filter in local.split_domain_filters :
+    domain_filter => "${var.release}-${replace(domain_filter, ".", "-")}"
+  }
 
   helm_values = {
     serviceAccount = {
       name = local.service_account_name
     }
-    domainFilters    = [var.domain_filters]
     provider         = var.dns_provider # https://github.com/kubernetes-sigs/external-dns/blob/5806e3474f2e13254498bd2af34302a4e283ae39/.github/labeler.yml
     additionalLabels = var.md_metadata.default_tags
   }
 }
 
 resource "helm_release" "external-dns" {
-  name             = var.release
+  for_each         = toset(local.split_domain_filters)
+  name             = local.release_name_map[each.key]
   chart            = "external-dns"
   repository       = "https://kubernetes-sigs.github.io/external-dns/"
   version          = "1.7.1"
@@ -23,6 +28,7 @@ resource "helm_release" "external-dns" {
   values = [
     "${file("${path.module}/values.yaml")}",
     yamlencode(local.helm_values),
-    yamlencode(var.helm_additional_values)
+    yamlencode(var.helm_additional_values),
+    yamlencode({ domainFilters = each.value })
   ]
 }
