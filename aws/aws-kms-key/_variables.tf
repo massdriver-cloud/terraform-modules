86144eeaec68eacaecbe36959c7f39b2ceb995fd
Chris Hill <chris@massdriver.cloud>
2022-07-21 17:00:01 -0700

    Create KMS module for use in all bundles that can use KMS

---
+++ aws/aws-kms-key/_variables.tf
@@ -0,0 +1,26 @@
+variable "md_metadata" {
+  description = "Massdriver package metadata object"
+  type        = any
+}
+
+variable "kms_encryption" {
+  description = "AWS KMS encryption parameter block"
+  type        = any
+}
+
+variable "key_alias_context" {
+  description = "(Optional) Additional string context to append to the end of the key alias. This is useful in bundles where multiple keys are created and you need to give context to differentiate each key. Must be lowercase letters and hyphens without spaces and no more than 20. If this field is not empty, it will be appended to the end of the alias with a hyphen"
+  type        = string
+  default     = ""
+
+  validation {
+    # regex(...) fails if it cannot find a match
+    condition     = can(regex("^([a-z]{0,20}|[a-z][a-z0-9-]{0,19})$", var.key_alias_context))
+    error_message = "The key_alias_context variable must begin with a letter, contain only lowercase letters, numbers or hyphens and be 20 characters or less."
+  }
+}
+
+variable "policy" {
+  description = "IAM policy to apply to the KMS key"
+  type        = string
+}
