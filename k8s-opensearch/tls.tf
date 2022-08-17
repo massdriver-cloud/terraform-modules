e8df37b64e1b272c08fd81d9bcc8c55147acf7bf
Jake Ferriero <jake@massdriver.cloud>
2022-08-17 11:37:38 -0700

    fixup tls ref

--- k8s-opensearch/tls.tf
+++ k8s-opensearch/tls.tf
@@ -7,7 +7,7 @@ resource "tls_private_key" "opensearch" {
 
 resource "tls_self_signed_cert" "opensearch" {
   count = true ? 1 : 0
-  private_key_pem = tls_private_key.opensearch.private_key_pem
+  private_key_pem = tls_private_key.opensearch[0].private_key_pem
   subject {
     common_name  = "massdriver.cloud"
     organization = "MassDriver Inc"
