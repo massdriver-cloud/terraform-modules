585231a8e59a877c2fa0bdaa0b4e828b8d287091
Will <will@containerlabs.io>
2022-08-18 13:29:14 -0700

    tostring for envs

--- k8s-application/envs.tf
+++ k8s-application/envs.tf
@@ -6,13 +6,13 @@ locals {
     split(".", env_path)[0] == "connections" ? [{ name = env_key, value = ".${join(".", slice(split(".", env_path), 1, length(split(".", env_path))))}" }] : []
   ]])
   mapped_envs_connections = { for env in local.merged_envs_connections : env.name => env.value }
-  result_envs_connections = [for name, value in local.mapped_envs_connections : { name = name, value = jsondecode(data.jq_query.envs_connections["${name}"].result) }]
+  result_envs_connections = [for name, value in local.mapped_envs_connections : { name = name, value = tostring(jsondecode(data.jq_query.envs_connections["${name}"].result)) }]
 
   merged_envs_params = flatten([for env_key, env_path in local.app_envs : [
     split(".", env_path)[0] == "params" ? [{ name = env_key, value = ".${join(".", slice(split(".", env_path), 1, length(split(".", env_path))))}" }] : []
   ]])
   mapped_envs_params = { for env in local.merged_envs_params : env.name => env.value }
-  result_envs_params = [for name, value in local.mapped_envs_params : { name = name, value = jsondecode(data.jq_query.envs_params["${name}"].result) }]
+  result_envs_params = [for name, value in local.mapped_envs_params : { name = name, value = tostring(jsondecode(data.jq_query.envs_params["${name}"].result)) }]
   result_envs        = concat(local.result_envs_connections, local.result_envs_params)
 }
 
