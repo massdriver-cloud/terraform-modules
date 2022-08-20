12378c5d4b0cea6182e214d839d73050927d8b57
Cory O'Daniel <cory@coryodaniel.com>
2022-08-20 14:25:06 -0700

    pre-commit

--- massdriver-application/variables.tf
+++ massdriver-application/variables.tf
@@ -7,3 +7,8 @@ variable "identity" {
   description = "The identity this application wil assume. Assume Role Policy for IAM ... TODO"
   type        = any
 }
+
+variable "root_path" {
+  description = "Path to root module. This is used to load massdriver.yaml and params/connection variables."
+  type        = string
+}
