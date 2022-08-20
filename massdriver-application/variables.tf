5be97f94e9b411f98200a48dc3157fb3209b72d2
Cory O'Daniel <cory@coryodaniel.com>
2022-08-20 14:41:08 -0700

    try try try

--- massdriver-application/variables.tf
+++ massdriver-application/variables.tf
@@ -7,8 +7,3 @@ variable "identity" {
   description = "The identity this application wil assume. Assume Role Policy for IAM ... TODO"
   type        = any
 }
-
-variable "root_path" {
-  description = "Path to root module. This is used to load massdriver.yaml and params/connection variables."
-  type        = string
-}
