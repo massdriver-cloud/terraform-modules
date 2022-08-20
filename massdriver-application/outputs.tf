82411b6cc97f8ca593cec0bddf4fed590a099025
Cory O'Daniel <cory@coryodaniel.com>
2022-08-20 15:20:53 -0700

    addl outputs

--- massdriver-application/outputs.tf
+++ massdriver-application/outputs.tf
@@ -12,3 +12,11 @@ output "envs" {
   description = "The policies parsed from massdriver.yaml"
   value       = local.envs
 }
+
+output "params" {
+  # We provide these as an output as its needed for passing into runtimes (helm, etc)
+  # and we don't want end-developers to have to parse the write files to get them since
+  # we've already done the work
+  description = "Parameters provided to bundle."
+  value       = local.params
+}
