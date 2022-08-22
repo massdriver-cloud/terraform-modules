0091dd309e499b73192fa6febb35a8cc602b7320
Cory O'Daniel <cory@coryodaniel.com>
2022-08-22 16:34:43 -0700

    service var

--- massdriver-application/aws_identity.tf
+++ massdriver-application/aws_identity.tf
@@ -5,7 +5,10 @@ locals {
     kubernetes = "eks"
   }
 
-  aws_service = local.aws_service_map[var.service]
+  aws_service                      = local.aws_service_map[var.service]
+  aws_eks_oidc_short               = local.is_aws && local.is_kubernetes ? replace(var.kubernetes.cluster_artifact.data.infrastructure.oidc_issuer_url, "https://", "") : null
+  aws_eks_oidc_federated_principal = local.is_aws && local.is_kubernetes ? "arn:aws:iam::${data.mdxc_cloud.current.id}:oidc-provider/${local.aws_eks_oidc_short}" : null
+  aws_eks_assume_role_conditional  = local.is_aws && local.is_kubernetes ? "system:serviceaccount:${var.kubernetes.namespace}:${var.name}" : null
 
   aws_identity = {
     assume_role_policy = <<EOF
@@ -28,9 +31,25 @@ locals {
 EOF
   }
 
-  aws_eks_oidc_short               = local.is_aws && local.is_kubernetes ? replace(var.kubernetes.cluster_artifact.data.infrastructure.oidc_issuer_url, "https://", "") : null
-  aws_eks_oidc_federated_principal = local.is_aws && local.is_kubernetes ? "arn:aws:iam::${data.mdxc_cloud.current.id}:oidc-provider/${local.eks_oidc_short}" : null
-  aws_eks_assume_role_conditional  = local.is_aws && local.is_kubernetes ? "system:serviceaccount:${var.kubernetes.namespace}:${var.name}" : null
+  #   eks_assume_role_policy = <<EOF
+  # {
+  #   "Version": "2012-10-17",
+  #   "Statement": [
+  #     {
+  #       "Effect": "Allow",
+  #       "Action": [
+  #         "sts:AssumeRole"
+  #       ],
+  #       "Principal": {
+  #         "Service": [
+  #           "${local.aws_service}.amazonaws.com"
+  #         ]
+  #       }
+  #     }
+  #   ]
+  # }  
+  # EOF
+
 }
 
 
