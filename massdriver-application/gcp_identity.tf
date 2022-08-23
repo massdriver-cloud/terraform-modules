231a701e3c9dd9f1725c623d7de6d934204f1089
Cory O'Daniel <cory@coryodaniel.com>
2022-08-23 16:28:06 -0700

    service var

--- massdriver-application/gcp_identity.tf
+++ massdriver-application/gcp_identity.tf
@@ -1,10 +1,12 @@
 # For GCP individual service receive a service_account email to bind what they run as, so we don't need to do anything
 # in the non-GKE scenario, for GKE, we need to build a workload identity. MDXC has all of the data it needs besides the
 
-# TODO: Set the below fields
 locals {
-  gcp_identity = var.service == "kubernetes" ? {
-    namespace       = "TODO"
-    service_account = "TODO"
+  gcp_identity = local.is_kubernetes ? {
+    # TODO: uncomment once mdxc expects gcp_configuration w/ k8s sub object
+    # kubernetes = {
+    #   namespace            = var.kubernetes.namespace
+    #   service_account_name = var.name
+    # }
   } : {}
 }
