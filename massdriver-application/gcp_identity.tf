1d9df8e0fd7bc3520af9cb17df178f951c068fb3
Cory O'Daniel <cory@coryodaniel.com>
2022-08-23 16:23:15 -0700

    service var

--- massdriver-application/gcp_identity.tf
+++ massdriver-application/gcp_identity.tf
@@ -1,5 +1,5 @@
 # For GCP individual service receive a service_account email to bind what they run as, so we don't need to do anything
-# in the non-GKE scenario, for GKE, we need to build a workload identity. MDXC has all of the data it needs besides the 
+# in the non-GKE scenario, for GKE, we need to build a workload identity. MDXC has all of the data it needs besides the
 
 # TODO: Set the below fields
 locals {
