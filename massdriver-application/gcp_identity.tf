badcd423648be345e47825336cca79aa99483cc2
Will Beebe <will@containerlabs.io>
2022-08-28 15:14:23 -0700

    mdxc now has gcp k8s support

--- massdriver-application/gcp_identity.tf
+++ massdriver-application/gcp_identity.tf
@@ -1,12 +1,12 @@
 # For GCP individual service receive a service_account email to bind what they run as, so we don't need to do anything
 # in the non-GKE scenario, for GKE, we need to build a workload identity. MDXC has all of the data it needs besides the
+# namespace and k8s service account name
 
 locals {
   gcp_identity = local.is_kubernetes ? {
-    # TODO: uncomment once mdxc expects gcp_configuration w/ k8s sub object
-    # kubernetes = {
-    #   namespace            = var.kubernetes.namespace
-    #   service_account_name = var.name
-    # }
+    kubernetes = {
+      namespace            = var.kubernetes.namespace
+      service_account_name = var.name
+    }
   } : {}
 }
