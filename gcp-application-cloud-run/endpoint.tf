f02f1ec05cf2ca0bc99bdf9b00b25a1261faf874
Will <will@containerlabs.io>
2022-08-29 16:20:06 -0700

    better names

--- gcp-application-cloud-run/endpoint.tf
+++ gcp-application-cloud-run/endpoint.tf
@@ -1,4 +1,4 @@
-module "gcp_dns_ssl" {
+module "serverless_endpoint" {
   count                  = var.endpoint != null ? 1 : 0
   source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint"
   rresource_name         = module.application.params.md_metadata.name_prefix
