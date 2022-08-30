872e3971057d93ec8b43513f00a88658ce614d69
Will <will@containerlabs.io>
2022-08-30 13:48:34 -0700

    bump version of endpoint module

--- massdriver-application-gcp-cloud-run/endpoint.tf
+++ massdriver-application-gcp-cloud-run/endpoint.tf
@@ -1,6 +1,6 @@
 module "serverless_endpoint" {
   count                  = var.endpoint != null ? 1 : 0
-  source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint?ref=adbd06e"
+  source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint?ref=7264e10"
   resource_name          = module.application.params.md_metadata.name_prefix
   zone                   = var.endpoint.zone
   subdomain              = var.endpoint.subdomain
