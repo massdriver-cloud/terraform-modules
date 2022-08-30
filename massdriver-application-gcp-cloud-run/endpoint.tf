790959371fd8a6f242ab9d30ccf0699b0266b08c
Will <will@containerlabs.io>
2022-08-30 13:53:25 -0700

    bump endpoint

--- massdriver-application-gcp-cloud-run/endpoint.tf
+++ massdriver-application-gcp-cloud-run/endpoint.tf
@@ -1,6 +1,6 @@
 module "serverless_endpoint" {
   count                  = var.endpoint != null ? 1 : 0
-  source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint?ref=7264e10"
+  source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint?ref=8c4cd0b"
   resource_name          = module.application.params.md_metadata.name_prefix
   labels                 = module.application.params.md_metadata.default_tags
   zone                   = var.endpoint.zone
