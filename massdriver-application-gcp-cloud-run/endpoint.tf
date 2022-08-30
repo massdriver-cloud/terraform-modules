adbd06ee036f59561f5f5226c41dbf28486dd719
Will <will@containerlabs.io>
2022-08-30 10:30:58 -0700

    fix typo in argument name

--- massdriver-application-gcp-cloud-run/endpoint.tf
+++ massdriver-application-gcp-cloud-run/endpoint.tf
@@ -1,7 +1,7 @@
 module "serverless_endpoint" {
   count                  = var.endpoint != null ? 1 : 0
   source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint"
-  rresource_name         = module.application.params.md_metadata.name_prefix
+  resource_name          = module.application.params.md_metadata.name_prefix
   zone                   = var.endpoint.zone
   subdomain              = var.endpoint.subdomain
   location               = var.location
