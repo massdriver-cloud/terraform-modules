ef6c4d963c6fad8fb5e562a9257b57d46af1d166
Will <will@containerlabs.io>
2022-08-30 13:50:41 -0700

    add labels

--- massdriver-application-gcp-cloud-run/endpoint.tf
+++ massdriver-application-gcp-cloud-run/endpoint.tf
@@ -2,6 +2,7 @@ module "serverless_endpoint" {
   count                  = var.endpoint != null ? 1 : 0
   source                 = "github.com/massdriver-cloud/terraform-modules//gcp-serverless-endpoint?ref=7264e10"
   resource_name          = module.application.params.md_metadata.name_prefix
+  labels                 = module.application.params.md_metadata.default_tags
   zone                   = var.endpoint.zone
   subdomain              = var.endpoint.subdomain
   location               = var.location
