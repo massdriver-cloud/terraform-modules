3eb16ddbdebe9a0321931bd8b459c07a63805db9
Jake Ferriero <jake@massdriver.cloud>
2022-08-31 15:46:40 -0700

    K8s fluentbit (#21)
    * wip fluentd
    
    * make modules pass fileCofigs as additional values because these may really vary and need things like opensearch password in them
    
    * remove unused var
    
    * make users set plugins they need
    
    * add fluentbit module
    
    * remove opensearch env var
    
    * remove fluentd
    
    * fixup chart ref
    
    * review feedback
    
    * remove outputs

---
+++ k8s-fluentbit/main.tf
@@ -0,0 +1,31 @@
+locals {
+  # this should be the default calculated name anyway, but we want to enforce it just to be sure
+  service_account_name = var.release == "fluentbit" ? "fluentbit" : "${var.release}-fluentbit"
+
+  # extract data about kubernetes cluster
+  cloud            = var.kubernetes_cluster.specs.kubernetes.cloud
+  k8s_distribution = var.kubernetes_cluster.specs.kubernetes.distribution
+
+  helm_values = {
+    serviceAccount = {
+    name = local.service_account_name
+    }
+    labels = var.md_metadata.default_tags
+  }
+}
+
+resource "helm_release" "fluentbit" {
+  name             = var.release
+  chart            = "fluent-bit"
+  repository       = "https://fluent.github.io/helm-charts"
+  version          = "v0.20.6"
+  namespace        = var.namespace
+  create_namespace = true
+  wait_for_jobs    = true
+
+  values = [
+    "${file("${path.module}/values.yaml")}",
+    yamlencode(local.helm_values),
+    yamlencode(var.helm_additional_values)
+  ]
+}
