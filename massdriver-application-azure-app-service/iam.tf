0fd8fd13ac6f8745bd0e2a2febf63a91d920fa39
mclacore <michael@massdriver.cloud>
2022-09-08 16:27:07 -0600

    Hotfix for service principal auth

--- massdriver-application-azure-app-service/iam.tf
+++ massdriver-application-azure-app-service/iam.tf
@@ -2,5 +2,5 @@
 resource "azurerm_role_assignment" "app_read_acr" {
   scope                = data.azurerm_container_registry.main.id
   role_definition_name = "AcrPull"
-  principal_id         = module.application.id
+  principal_id         = module.application.azure_application_identity.service_principal_object_id
 }
