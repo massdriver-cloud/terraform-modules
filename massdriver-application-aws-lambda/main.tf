c5656918d43cbc0bb13c3b1a0ad2d0f8cb11880b
dwill <xpositivityx@gmail.com>
2022-09-09 11:44:20 -0700

    massdriver application using the lambda module

--- massdriver-application-aws-lambda/main.tf
+++ massdriver-application-aws-lambda/main.tf
@@ -9,55 +9,14 @@ module "application" {
   service = "function"
 }
 
-resource "aws_lambda_function" "main" {
-  function_name = var.md_metadata.name_prefix
-  role          = module.application.id
-  image_uri     = "${var.image.uri}:${var.image.tag}"
-  package_type  = "Image"
-  publish       = true
-  memory_size   = var.memory_size
-  timeout       = var.execution_timeout
-
-  environment {
-    variables = module.application.envs
-  }
-
-  dynamic "tracing_config" {
-    for_each = var.x_ray_enabled ? [1] : []
-    content {
-      mode = "Active"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_group" "function_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.main.function_name}"
-  retention_in_days = var.retention_days
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-resource "aws_iam_policy" "function_logging_policy" {
-  name   = "${aws_lambda_function.main.function_name}-logging-policy"
-  policy = <<EOF
-{
-  "Version" : "2012-10-17",
-  "Statement" : [
-    {
-      "Action" : [
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Effect" : "Allow",
-      "Resource" : "${aws_cloudwatch_log_group.function_log_group.arn}"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
-  role       = local.role_name
-  policy_arn = aws_iam_policy.function_logging_policy.arn
+module "aws_lambda_function" "main" {
+  source         = "github.com/massdriver-cloud/terraform-modules//aws/aws-lambda-function"
+  function_name  = var.md_metadata.name_prefix
+  role_arn       = module.application.id
+  image          = var.image
+  memory_size    = var.memory_size
+  timeout        = var.execution_timeout
+  envs           = module.application.envs
+  x_ray_enabled  = var.x-ray.enabled
+  retention_days = var.retention_days
 }
