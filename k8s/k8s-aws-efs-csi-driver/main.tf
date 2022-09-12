f6f23bdfc7a4f1b80150b7cb9cc87b6a73a86c62
chrisghill <chris@massdriver.cloud>
2022-09-11 18:58:26 -0600

    add k8s-aws-efs-csi-driver module

---
+++ k8s/k8s-aws-efs-csi-driver/main.tf
@@ -0,0 +1,21 @@
+locals {
+  // Resource names (other than the service accounts) in the helm chart don't support templatization right now
+  // so so hardcoding the service account names to match
+  controller_service_account_name = "efs-csi-controller"
+  node_service_account_name = "efs-csi-node"
+  chart_version = "2.2.7"
+}
+
+resource "helm_release" "main" {
+  name             = var.release
+  chart            = "aws-efs-csi-driver"
+  repository       = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+  version          = local.chart_version
+  namespace        = var.namespace
+
+  values = [
+    "${file("${path.module}/values.yaml")}",
+    yamlencode(local.helm_values),
+    yamlencode(var.helm_additional_values)
+  ]
+}
