1137d6063ebcde43fd4b8d75168198adc6e0445a
mclacore <michael@massdriver.cloud>
2022-09-12 16:38:14 -0600

    Consolidating single-resource files

--- massdriver-application-azure-app-service/iam.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "main" {
-  scope                = data.azurerm_container_registry.main.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.main.identity[0].principal_id
-}
--- massdriver-application-azure-app-service/main.tf
+++ massdriver-application-azure-app-service/main.tf
@@ -162,3 +162,9 @@ resource "azurerm_linux_web_app" "main" {
 
   tags = var.tags
 }
+
+resource "azurerm_role_assignment" "main" {
+  scope                = data.azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.main.identity[0].principal_id
+}
