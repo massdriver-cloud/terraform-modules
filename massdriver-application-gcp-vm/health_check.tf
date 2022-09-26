6c9fb68f332a9c04ad6050fb42e4f79edd9ddf0a
Will <will@containerlabs.io>
2022-09-26 14:03:03 -0700

    gcp vm module v0.1

--- massdriver-application-gcp-vm/health_check.tf
+++ massdriver-application-gcp-vm/health_check.tf
@@ -1,14 +1,12 @@
-# # We may be able to remove this, see comment in main.tf
-# resource "google_compute_health_check" "autohealing" {
-#   name                = "${module.application.params.md_metadata.name_prefix}-autohealing"
-#   check_interval_sec  = 5
-#   timeout_sec         = 5
-#   healthy_threshold   = 2
-#   unhealthy_threshold = 10 # 50 seconds
+resource "google_compute_health_check" "autohealing" {
+  name                = "${module.application.params.md_metadata.name_prefix}-autohealing"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10 # 50 seconds
 
-#   http_health_check {
-#     request_path = "/"
-#     # TODO: make this configurable
-#     port = "80"
-#   }
-# }
+  http_health_check {
+    request_path = "/"
+    port         = var.port
+  }
+}
--- massdriver-application-gcp-vm/monitoring.tf
+++ massdriver-application-gcp-vm/monitoring.tf
@@ -1,33 +1,33 @@
-# locals {
-#   threshold_cpu = 0.9
-#   metrics = {
-#     "cpu" = {
-#       # metric for each instance or the whole group?
-#       metric   = "compute.googleapis.com/instance/cpu/utilization"
-#       resource = "gce_instance"
-#     }
-#   }
-# }
+locals {
+  threshold_cpu = 0.9
+  metrics = {
+    "cpu" = {
+      # metric for each instance or the whole group?
+      metric   = "compute.googleapis.com/instance/cpu/utilization"
+      resource = "gce_instance"
+    }
+  }
+}
 
-# module "alarm_channel" {
-#   source      = "github.com/massdriver-cloud/terraform-modules//gcp-alarm-channel?ref=bfcf556"
-#   md_metadata = module.application.params.md_metadata
-# }
+module "alarm_channel" {
+  source      = "github.com/massdriver-cloud/terraform-modules//gcp-alarm-channel?ref=bfcf556"
+  md_metadata = module.application.params.md_metadata
+}
 
-# module "cpu_alarm" {
-#   source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=cafdc89"
-#   notification_channel_id = module.alarm_channel.id
-#   md_metadata             = module.application.params.md_metadata
-#   display_name            = "CPU Usage High"
-#   message                 = "Cloud SQL Postgres ${google_redis_instance.redis.id}: Memory capacity over threshold ${local.threshold_memory * 100}%"
-#   alarm_name              = "highMemory"
-#   metric_type             = local.metrics["cpu"].metric
-#   resource_type           = local.metrics["cpu"].resource
-#   threshold               = local.threshold_cpu
-#   period                  = 60
-#   duration                = 60
+module "cpu_alarm" {
+  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=cafdc89"
+  notification_channel_id = module.alarm_channel.id
+  md_metadata             = module.application.params.md_metadata
+  display_name            = "CPU Usage High"
+  message                 = "CPU usage over threshold ${local.threshold_cpu * 100}%"
+  alarm_name              = "highMemory"
+  metric_type             = local.metrics["cpu"].metric
+  resource_type           = local.metrics["cpu"].resource
+  threshold               = local.threshold_cpu
+  period                  = 60
+  duration                = 60
 
-#   depends_on = [
-#     google_redis_instance.redis
-#   ]
-# }
+  depends_on = [
+    google_compute_instance_group_manager.main
+  ]
+}
