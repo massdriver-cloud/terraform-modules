3ee9d5fa9ab976a4eec303966803b252c293f0a2
Will <will@containerlabs.io>
2022-10-12 20:18:07 -0700

    PR feedback

--- massdriver-application-gcp-cloud-function/files/python310/main.py
+++ massdriver-application-gcp-cloud-function/files/python310/main.py
@@ -1,26 +1,25 @@
-import logging
-import os
-
-import redis
-from pyramid.config import Configurator
-from pyramid.response import Response
-
-log = logging.getLogger(__name__)
-r = redis.Redis(
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    password=os.getenv('DB_PASSWORD'),
-    db=0)
+from flask import escape
+import functions_framework
 
 
-def hello_world(request):
-    print('hello world')
-    r.set('hello', 'world')
-    return Response('Pyramid! Hello <strong>' + r.get('hello').decode('utf-8') + '</strong>')
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
-
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.add_route('hello', '/')
-    config.add_view(hello_world, route_name='hello')
-    return config.make_wsgi_app()
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
