d38bb902558a89817f3d1bfe2b11ebf4e9629720
Will <will@containerlabs.io>
2022-10-25 19:55:23 -0700

    work in progress

--- massdriver-application-helm/main.tf
+++ massdriver-application-helm/main.tf
@@ -1,14 +1,48 @@
+# locals {
+#   // Combine environment variables from application and module variables (params)
+
+
+#   aws_additional_values = merge(local.base_helm_additional_values, {
+#     serviceAccount = {
+#       annotations = {
+#         "eks.amazonaws.com/role-arn" = module.application.id
+#       }
+#     }
+#   })
+
+#   gcp_additional_values = merge(local.base_helm_additional_values, {
+#     serviceAccount = {
+#       annotations = {
+#         "iam.gke.io/gcp-service-account" = module.application.id
+#       }
+#     }
+#   })
+
+#   # TODO: Add azure, I dont think this has an annotation equiv, will probably be ENV Vars w/ secret
+#   azure_additional_values = merge(local.base_helm_additional_values, {})
+
+#   cloud_specific_helm_additional_values = var.chart_repository != null ? {} : {
+#     aws   = local.aws_additional_values,
+#     gcp   = local.gcp_additional_values,
+#     azure = local.azure_additional_values
+#   }
+
+#   helm_additional_values = local.cloud_specific_helm_additional_values[module.application.cloud]
+# }
+
+
+
 locals {
   // Combine environment variables from application and module variables (params)
   combined_envs = merge(
-    module.application.envs,
+    module.helm_values.envs,
     { for env in var.additional_envs : env.name => env.value }
   )
-  base_helm_additional_values = var.chart_repository != null ? {} : {
-    commonLabels = module.application.params.md_metadata.default_tags
+  base_helm_additional_values = {
+    commonLabels = module.helm_values.common_labels
     pod = {
       annotations = {
-        "md-deployment-id" = lookup(module.application.params.md_metadata.deployment, "id", "")
+        "md-deployment-id" = module.helm_values.md_deployment_id
       }
     }
     envs = [for key, val in local.combined_envs : { name = key, value = tostring(val) }]
@@ -40,24 +74,21 @@ locals {
   # TODO: Add azure, I dont think this has an annotation equiv, will probably be ENV Vars w/ secret
   azure_additional_values = merge(local.base_helm_additional_values, {})
 
-  cloud_specific_helm_additional_values = var.chart_repository != null ? {} : {
+  cloud_specific_helm_additional_values = {
     aws   = local.aws_additional_values,
     gcp   = local.gcp_additional_values,
     azure = local.azure_additional_values
   }
 
-  helm_additional_values = local.cloud_specific_helm_additional_values[module.application.cloud]
+  helm_additional_values = local.cloud_specific_helm_additional_values[var.mdx_application.cloud]
 }
 
-module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
-  name    = var.name
-  service = "kubernetes"
 
-  kubernetes = {
-    namespace        = var.namespace
-    cluster_artifact = var.kubernetes_cluster
-  }
+module "helm_values" {
+  source             = "../massdriver-helm-values"
+  name               = var.name
+  namespace          = var.namespace
+  kubernetes_cluster = var.kubernetes_cluster
 }
 
 resource "helm_release" "application" {
@@ -72,7 +103,7 @@ resource "helm_release" "application" {
 
   values = [
     fileexists("${var.chart}/values.yaml") ? file("${var.chart}/values.yaml") : "",
-    yamlencode(module.application.params),
-    yamlencode(local.helm_additional_values)
+    yamlencode(module.helm_values.application.params),
+    yamlencode(module.helm_values.helm_additional_values)
   ]
 }
