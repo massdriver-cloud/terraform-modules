fb0fd1d2089c6d1cc5235028fa127603223df933
Will <will@containerlabs.io>
2022-10-26 20:13:00 -0700

    v2

--- massdriver-application-helm/main.tf
+++ massdriver-application-helm/main.tf
@@ -1,44 +1,10 @@
-# locals {
-#   // Combine environment variables from application and module variables (params)
-
-
-#   aws_additional_values = merge(local.base_helm_additional_values, {
-#     serviceAccount = {
-#       annotations = {
-#         "eks.amazonaws.com/role-arn" = module.application.id
-#       }
-#     }
-#   })
-
-#   gcp_additional_values = merge(local.base_helm_additional_values, {
-#     serviceAccount = {
-#       annotations = {
-#         "iam.gke.io/gcp-service-account" = module.application.id
-#       }
-#     }
-#   })
-
-#   # TODO: Add azure, I dont think this has an annotation equiv, will probably be ENV Vars w/ secret
-#   azure_additional_values = merge(local.base_helm_additional_values, {})
-
-#   cloud_specific_helm_additional_values = var.chart_repository != null ? {} : {
-#     aws   = local.aws_additional_values,
-#     gcp   = local.gcp_additional_values,
-#     azure = local.azure_additional_values
-#   }
-
-#   helm_additional_values = local.cloud_specific_helm_additional_values[module.application.cloud]
-# }
-
-
-
 locals {
   // Combine environment variables from application and module variables (params)
   combined_envs = merge(
     module.helm_values.envs,
     { for env in var.additional_envs : env.name => env.value }
   )
-  base_helm_additional_values = {
+  helm_additional_values = {
     commonLabels = module.helm_values.common_labels
     pod = {
       annotations = {
@@ -53,36 +19,9 @@ locals {
         "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true" // TODO: hardcoding this for now, dependent on nginx
       }
     }
+    serviceAccount = module.helm_values.k8s_service_account
   }
-
-  aws_additional_values = merge(local.base_helm_additional_values, {
-    serviceAccount = {
-      annotations = {
-        "eks.amazonaws.com/role-arn" = module.application.id
-      }
-    }
-  })
-
-  gcp_additional_values = merge(local.base_helm_additional_values, {
-    serviceAccount = {
-      annotations = {
-        "iam.gke.io/gcp-service-account" = module.application.id
-      }
-    }
-  })
-
-  # TODO: Add azure, I dont think this has an annotation equiv, will probably be ENV Vars w/ secret
-  azure_additional_values = merge(local.base_helm_additional_values, {})
-
-  cloud_specific_helm_additional_values = {
-    aws   = local.aws_additional_values,
-    gcp   = local.gcp_additional_values,
-    azure = local.azure_additional_values
-  }
-
-  helm_additional_values = local.cloud_specific_helm_additional_values[var.mdx_application.cloud]
 }
-
 
 module "helm_values" {
   source             = "../massdriver-helm-values"
@@ -104,6 +43,6 @@ resource "helm_release" "application" {
   values = [
     fileexists("${var.chart}/values.yaml") ? file("${var.chart}/values.yaml") : "",
     yamlencode(module.helm_values.application.params),
-    yamlencode(module.helm_values.helm_additional_values)
+    yamlencode(local.helm_additional_values)
   ]
 }
