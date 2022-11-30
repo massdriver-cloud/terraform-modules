683dff7d2873cbd4a16789b2e976ba60bd658e73
Will <will@containerlabs.io>
2022-11-29 19:35:17 -0800

    gcp metric alarm

---
+++ gcp-metric-alarm/threshold_alarm.tf
@@ -0,0 +1,34 @@
+locals {
+  is_theshold_alarm = var.alarm_configuration.value_type == "DOUBLE" || var.alarm_configuration.value_type == "INT64"
+}
+
+resource "google_monitoring_alert_policy" "main" {
+  count        = local.is_theshold_alarm ? 1 : 0
+  display_name = local.alarm_unique_id
+  combiner     = "OR"
+  conditions {
+    display_name = "${var.md_metadata.name_prefix} COMPARISON_GT"
+    condition_threshold {
+      filter     = local.filter
+      duration   = "${var.alarm_configuration.duration_s}s"
+      comparison = "COMPARISON_GT"
+      aggregations {
+        alignment_period     = "${var.alarm_configuration.alignment_period_s}s"
+        cross_series_reducer = "REDUCE_MEAN"
+        per_series_aligner   = "ALIGN_MAX"
+      }
+      # should be able to use for numbers too
+      threshold_value = var.alarm_configuration.threshold
+
+      trigger {
+        count = 1
+      }
+    }
+  }
+
+  notification_channels = [
+    var.notification_channel_id
+  ]
+
+  user_labels = var.md_metadata.default_tags
+}
---
+++ gcp-metric-alarm/variables.tf
@@ -0,0 +1,48 @@
+variable "alarm_configuration" {
+  type = object({
+    metric_type        = string
+    resource_type      = string
+    threshold          = number
+    duration_s         = optional(number, 60)
+    alignment_period_s = optional(number, 60)
+    value_type         = optional(string, "DOUBLE")
+  })
+  validation {
+    condition     = var.alarm_configuration.duration_s >= 60
+    error_message = "The duration must be at least 60 seconds, it's currently ${var.alarm_configuration.duration_s}."
+  }
+  validation {
+    condition     = var.alarm_configuration.alignment_period_s >= 60
+    error_message = "The alignment period must be at least 60 seconds, it's currently ${var.alarm_configuration.alignment_period_s}."
+  }
+  validation {
+    condition = {
+      "DOUBLE" = true
+      "INT64"  = true
+      "BOOL"   = true
+      "STRING" = true
+    }[var.alarm_configuration.value_type]
+    error_message = "${var.alarm_configuration.value_type} is not a valid value type."
+  }
+}
+
+# Massdriver metadata
+variable "md_metadata" {
+  type = any
+}
+
+# Massdriver variables
+variable "message" {
+  type        = string
+  description = "A message including additional context for this alarm."
+}
+
+variable "display_name" {
+  type        = string
+  description = "Short name to display in the massdriver UI."
+}
+
+variable "notification_channel_id" {
+  type        = string
+  description = "Massdriver Alarm Channel - Notification Channel ID"
+}
