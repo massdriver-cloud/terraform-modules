647289246ad7d815deb17d4dc79ccdaafe717aab
Will <will@containerlabs.io>
2022-12-08 15:06:22 -0800

    cleaning up alarm module, PR feedback

--- gcp-metric-alarm/bool_alarm.tf
+++ gcp-metric-alarm/bool_alarm.tf
@@ -1,15 +1,15 @@
 resource "google_monitoring_alert_policy" "bool" {
   count        = var.alarm_configuration.value_type == "BOOL" ? 1 : 0
-  display_name = local.alarm_unique_id
+  display_name = local.alarm_id
   combiner     = "OR"
   conditions {
-    display_name = "${var.md_metadata.name_prefix} BOOL"
+    display_name = var.display_name
     condition_threshold {
       filter   = local.filter
       duration = "${var.alarm_configuration.duration_s}s"
       # only COMPARISON_LT and COMPARISON_GT are supported
       comparison = "COMPARISON_GT"
-      # should be able to use for numbers too
+      # bool alarms are 0 or 1
       threshold_value = var.alarm_configuration.threshold
 
       trigger {
