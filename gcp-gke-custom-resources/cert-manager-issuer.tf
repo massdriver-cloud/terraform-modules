b3f344960a638e23e4d19167eaca177717e125bc
Will <will@containerlabs.io>
2022-12-31 11:20:02 -0800

    add custom resources module

---
+++ gcp-gke-custom-resources/cert-manager-issuer.tf
@@ -0,0 +1,45 @@
+locals {
+  enable_cert_manager = length(var.cloud_dns_managed_zones) > 0
+
+  managed_zones = [for zone in var.cloud_dns_managed_zones :
+    length(split("/", zone)) > 1 ? split("/", zone)[3] : zone
+  ]
+}
+
+data "google_dns_managed_zone" "hosted_zones" {
+  for_each = toset(local.managed_zones)
+  name     = each.key
+}
+
+resource "kubernetes_manifest" "cluster_issuer" {
+  count = local.enable_cert_manager ? 1 : 0
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1",
+    "kind"       = "ClusterIssuer",
+    "metadata" = {
+      "name" : "letsencrypt-prod"
+    },
+    "spec" = {
+      "acme" = {
+        "email" : "support+letsencrypt@massdriver.cloud"
+        "server" : "https://acme-v02.api.letsencrypt.org/directory"
+        "privateKeySecretRef" = {
+          "name" : "letsencrypt-prod-issuer-account-key"
+        },
+        "solvers" = concat([for name in local.managed_zones : {
+          "selector" = {
+            "dnsZones" = [
+              trimsuffix(data.google_dns_managed_zone.hosted_zones[name].dns_name, ".")
+            ]
+          },
+          "dns01" = {
+            "cloudDNS" = {
+              "project" : var.gcp_config.project_id,
+            }
+          }
+          }], [ // could put other solvers here
+        ])
+      }
+    }
+  }
+}
