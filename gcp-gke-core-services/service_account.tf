ae509088b7d383fd6d2f310a0f96f2a22af63924
Will <will@containerlabs.io>
2023-01-03 13:35:44 -0800

    creating namespace, passing to other modules

--- gcp-gke-core-services/service_account.tf
+++ gcp-gke-core-services/service_account.tf
@@ -1,13 +1,22 @@
+resource "kubernetes_namespace_v1" "md-core-services" {
+  metadata {
+    labels = var.md_metadata.default_tags
+    name   = local.core_services_namespace
+  }
+}
+
 resource "kubernetes_service_account_v1" "cloud_provisioner" {
   metadata {
-    name   = "massdriver-cloud-provisioner"
-    labels = var.md_metadata.default_tags
+    name      = "massdriver-cloud-provisioner"
+    labels    = var.md_metadata.default_tags
+    namespace = kubernetes_namespace_v1.md-core-services.metadata.0.name
   }
 }
 
 resource "kubernetes_secret_v1" "main" {
   metadata {
-    name = "massdriver-cloud-provisioner"
+    name      = "massdriver-cloud-provisioner"
+    namespace = kubernetes_namespace_v1.md-core-services.metadata.0.name
     annotations = {
       "kubernetes.io/service-account.name" = kubernetes_service_account_v1.cloud_provisioner.metadata.0.name
     }
@@ -28,6 +37,6 @@ resource "kubernetes_cluster_role_binding" "massdriver-cloud-provisioner" {
   subject {
     kind      = "ServiceAccount"
     name      = kubernetes_service_account_v1.cloud_provisioner.metadata.0.name
-    namespace = kubernetes_service_account_v1.cloud_provisioner.metadata.0.namespace
+    namespace = kubernetes_namespace_v1.md-core-services.metadata.0.name
   }
 }
