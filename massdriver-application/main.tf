f37b39ced8db65fa1e3bcc7814bfeac2630d56b7
Cory O'Daniel <cory@coryodaniel.com>
2023-01-09 11:46:10 -0800

    Secrets support

--- massdriver-application/main.tf
+++ massdriver-application/main.tf
@@ -2,23 +2,31 @@ locals {
   # These trys are here so that terraform validate will pass in CI.
   # We dont want to require the user to pass in the path to their MD bundle and requiring
   # everyone to parse the right files and pass them in is error prone. :tada:
-  app_specification = try(yamldecode(file("${path.root}/../massdriver.yaml")), {})
-  connections       = try(jsondecode(file("${path.root}/_connections.auto.tfvars.json")), {})
-  params            = try(jsondecode(file("${path.root}/_params.auto.tfvars.json")), {})
-  app_block         = lookup(local.app_specification, "app", {})
-  app_envs          = lookup(local.app_block, "envs", {})
-  app_policies      = toset(lookup(local.app_block, "policies", []))
+  app_specification    = try(yamldecode(file("${path.root}/../massdriver.yaml")), {})
+  connections          = try(jsondecode(file("${path.root}/_connections.auto.tfvars.json")), {})
+  params               = try(jsondecode(file("${path.root}/_params.auto.tfvars.json")), {})
+  secrets              = try(jsondecode(file("${path.root}/secrets.json")), {})
+  app_block            = lookup(local.app_specification, "app", {})
+  app_envs_queries     = lookup(local.app_block, "envs", {})
+  app_policies_queries = toset(lookup(local.app_block, "policies", []))
 
+  # package input fields to be exposed to JQ queries
   inputs_json = jsonencode({
     params      = local.params
     connections = local.connections
+    secrets     = local.secrets
   })
 
   application_identity_id = var.create_application_identity ? mdxc_application_identity.main.0.id : var.application_identity_id
 
-  policies = { for p in local.app_policies : p => jsondecode(data.jq_query.policies[p].result) }
+  policies = { for p in local.app_policies_queries : p => jsondecode(data.jq_query.policies[p].result) }
 
-  base_envs = { for k, v in local.app_envs : k => jsondecode(data.jq_query.envs[k].result) }
+  # env vars that have been resolved. additional vars may be added per cloud
+  base_envs = { for k, v in local.app_envs_queries : k => jsondecode(data.jq_query.envs[k].result) }
+    
+  # Auto generate ENV Vars for each secret
+  envs_with_secrets = merge(local.base_envs, local.secrets)
+    
   # Needed by Azure k8s until Workload Identity is out of preview.
   azure_envs = (local.is_azure_k8s && mdxc_application_identity.main[0] != null) ? mdxc_application_identity.main[0].azure_application_identity : {}
   azure_aks_envs = local.is_azure_k8s ? {
@@ -28,9 +36,9 @@ locals {
   } : {}
 
   cloud_envs = {
-    azure = merge(local.base_envs, local.azure_aks_envs)
-    aws   = local.base_envs
-    gcp   = local.base_envs
+    azure = merge(local.envs_with_secrets, local.azure_aks_envs)
+    aws   = local.envs_with_secrets
+    gcp   = local.envs_with_secrets
   }
 
   envs = local.cloud_envs[data.mdxc_cloud.current.cloud]
@@ -46,13 +54,13 @@ locals {
 }
 
 data "jq_query" "policies" {
-  for_each = local.app_policies
+  for_each = local.app_policies_queries
   data     = local.inputs_json
   query    = each.value
 }
 
 data "jq_query" "envs" {
-  for_each = local.app_envs
+  for_each = local.app_envs_queries
   data     = local.inputs_json
   query    = each.value
 }
