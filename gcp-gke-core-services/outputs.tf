0af251e942d0cd4056122635b86299cbfc17c5a5
chrisghill <chris@massdriver.cloud>
2023-01-15 21:13:46 -0700

    removed logging, fixed naming and references

--- gcp-gke-core-services/outputs.tf
+++ gcp-gke-core-services/outputs.tf
@@ -1,4 +1,11 @@
+
+resource "time_sleep" "wait_for_secret_token" {
+  depends_on      = [kubernetes_secret_v1.massdriver-cloud-provisioner_token]
+  create_duration = "10s"
+}
+
 output "kubernetes_token" {
-  value     = lookup(kubernetes_secret_v1.main.data, "token")
-  sensitive = true
+  value      = lookup(kubernetes_secret_v1.massdriver-cloud-provisioner_token.data, "token")
+  sensitive  = true
+  depends_on = [time_sleep.wait_for_secret_token]
 }
