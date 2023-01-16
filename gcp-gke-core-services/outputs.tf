252b21eb975010a74378dcecd3047035b41485f8
chrisghill <chris@massdriver.cloud>
2023-01-15 23:04:40 -0700

    Sleep was not the proper fix - needed 'wait_for_service_account_token'

--- gcp-gke-core-services/outputs.tf
+++ gcp-gke-core-services/outputs.tf
@@ -1,11 +1,5 @@
 
-resource "time_sleep" "wait_for_secret_token" {
-  depends_on      = [kubernetes_secret_v1.massdriver-cloud-provisioner_token]
-  create_duration = "10s"
-}
-
 output "kubernetes_token" {
-  value      = lookup(kubernetes_secret_v1.massdriver-cloud-provisioner_token.data, "token")
-  sensitive  = true
-  depends_on = [time_sleep.wait_for_secret_token]
+  value     = lookup(kubernetes_secret_v1.massdriver-cloud-provisioner_token.data, "token")
+  sensitive = true
 }
