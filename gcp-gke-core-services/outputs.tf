01ba923142980a047525e2f528aadb5563783da4
Christopher Hill <chris@massdriver.cloud>
2023-01-16 01:31:39 -0700

    Revert "Add data lookup to try to solve the token lookup (#104)"
    This reverts commit 9bf59012c5484007f8a1c112fe8d204a20bff4b6.

--- gcp-gke-core-services/outputs.tf
+++ gcp-gke-core-services/outputs.tf
@@ -1,5 +1,5 @@
 
 output "kubernetes_token" {
-  value     = lookup(data.kubernetes_secret_v1.massdriver-cloud-provisioner_token.data, "token")
+  value     = lookup(kubernetes_secret_v1.massdriver-cloud-provisioner_token.data, "token")
   sensitive = true
 }
