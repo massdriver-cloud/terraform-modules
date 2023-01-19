56a43c6a7bb08fe62729b73bc22fb6babce5273b
dwill <xpositivityx@gmail.com>
2023-01-19 01:41:19 -0800

    adding invoke arn, to lambda and root_resource_id to api gateway

--- massdriver-application-aws-lambda/_outputs.tf
+++ massdriver-application-aws-lambda/_outputs.tf
@@ -3,6 +3,11 @@ output "function_arn" {
   value       = module.aws_lambda_function.function_arn
 }
 
+output "function_invoke_arn" {
+  description = "The Amazon Resource Name of the invocation API"
+  value       = module.aws_lambda_function.invoke_arn
+}
+
 output "identity_arn" {
   description = "ARN of the workload identity created for the lambda function"
   value       = module.application.id
