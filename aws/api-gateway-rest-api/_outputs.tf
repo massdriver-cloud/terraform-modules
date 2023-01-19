2e621cf137c7c03e06f230b1dc2832df30aba7b3
dwill <xpositivityx@gmail.com>
2023-01-19 01:41:19 -0800

    adding invoke arn, to lambda and root_resource_id to api gateway

--- aws/api-gateway-rest-api/_outputs.tf
+++ aws/api-gateway-rest-api/_outputs.tf
@@ -7,3 +7,8 @@ output "stage_arn" {
   description = "The ARN of the API Gateway stage"
   value       = aws_api_gateway_stage.main.arn
 }
+
+output "root_resource_id" {
+  description = "The ID of the resource at the '/' route of the API Gateway"
+  value       = aws_api_gateway_rest_api.main.root_resource_id
+}
--- aws/aws-lambda-function/_outputs.tf
+++ aws/aws-lambda-function/_outputs.tf
@@ -2,3 +2,8 @@ output "function_arn" {
   description = "The amazon resource name of the function that was created"
   value       = aws_lambda_function.main.arn
 }
+
+output "invoke_arn" {
+  description = "The Amazon Resource Name of the invocation API"
+  value       = aws_lambda_function.main.invoke_arn
+}
