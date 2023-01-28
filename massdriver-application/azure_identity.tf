d823d083f1bc662b3e663a2e5d17a98ef2343ffb
Will <will@containerlabs.io>
2023-01-27 16:19:50 -0800

    managed identity changes

--- massdriver-application/azure_identity.tf
+++ massdriver-application/azure_identity.tf
@@ -1,6 +1,15 @@
-# TODO: Configure for azure.
-# This very well just might be {} since workload identity isnt generally available.
-
 locals {
-  azure_identity = {}
+  base_identity = {
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    kubernetes          = {}
+  }
+
+  azure_identity = local.is_kubernetes ? merge(local.base_identity, {
+    kubernetes = {
+      namespace            = var.kubernetes.namespace
+      service_account_name = var.name
+      oidc_issuer_url      = var.kubernetes.oidc_issuer_url
+    }
+  }) : local.base_identity
 }
