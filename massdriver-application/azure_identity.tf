feda9ed107be8141244e5ad8359654f89ee205e2
Will <will@containerlabs.io>
2023-01-27 19:26:30 -0800

    reducing complexity

--- massdriver-application/azure_identity.tf
+++ massdriver-application/azure_identity.tf
@@ -1,15 +1,11 @@
 locals {
-  base_identity = {
+  azure_identity = {
     location            = var.location
     resource_group_name = var.resource_group_name
-    kubernetes          = {}
-  }
-
-  azure_identity = local.is_kubernetes ? merge(local.base_identity, {
-    kubernetes = {
+    kubernetes = local.is_kubernetes ? {
       namespace            = var.kubernetes.namespace
       service_account_name = var.name
       oidc_issuer_url      = var.kubernetes.oidc_issuer_url
-    }
-  }) : local.base_identity
+    } : {}
+  }
 }
