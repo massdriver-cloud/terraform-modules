9fe6ed02f0c0ad703e01e42e91b57e0d6452acfc
Will <will@containerlabs.io>
2023-01-28 21:34:09 -0800

    resource_id

--- massdriver-application-azure-vm-docker/acr.tf
+++ massdriver-application-azure-vm-docker/acr.tf
@@ -4,5 +4,5 @@ data "azurerm_client_config" "main" {
 resource "azurerm_role_assignment" "acr" {
   scope                = "/subscriptions/${data.azurerm_client_config.main.subscription_id}"
   role_definition_name = "AcrPull"
-  principal_id         = module.application.identity.azure_application_identity.principal_id
+  principal_id         = module.application.identity.azure_application_identity.resource_id
 }
