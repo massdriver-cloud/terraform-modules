633f84a87f234c42250d433412a8d0f31c003516
Will <will@containerlabs.io>
2023-02-01 10:39:52 -0800

    remove default, rename output

--- azure/auto-cidr/variables.tf
+++ azure/auto-cidr/variables.tf
@@ -1,9 +1,5 @@
 variable "network_mask" {
-  # this tripped me up for a bit
-  # this has to be a number, required by the resource
-  # in some places we express this as a string prefixed with a slash: "/22"
-  type    = number
-  default = 22
+  type = number
 }
 
 variable "virtual_network_id" {
