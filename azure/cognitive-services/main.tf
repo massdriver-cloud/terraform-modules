46ce29b3920375e914431e73d7acee824f7fe288
mclacore <michael@massdriver.cloud>
2023-02-07 10:39:02 -0700

    Linked user assigned identity to service

--- azure/cognitive-services/main.tf
+++ azure/cognitive-services/main.tf
@@ -4,6 +4,13 @@ resource "azurerm_resource_group" "main" {
   tags     = var.tags
 }
 
+resource "azurerm_user_assigned_identity" "main" {
+  name                = var.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
+}
+
 resource "azurerm_cognitive_account" "main" {
   name                  = var.name
   location              = azurerm_resource_group.main.location
@@ -12,11 +19,9 @@ resource "azurerm_cognitive_account" "main" {
   sku_name              = var.sku_name
   custom_subdomain_name = var.custom_subdomain_name
   tags                  = var.tags
-}
 
-resource "azurerm_user_assigned_identity" "main" {
-  name                = var.name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  tags                = var.tags
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.main.id]
+  }
 }
