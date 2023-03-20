d43ddf4f31ae70ffdf80eacb5d20c2746923267f
chrisghill <chris@massdriver.cloud>
2023-03-20 16:21:06 -0600

    Bump module version for AWS applications

--- massdriver-application-aws-lambda/main.tf
+++ massdriver-application-aws-lambda/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application?ref=fc5f7b1"
+  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application?ref=36f3357"
   name    = var.md_metadata.name_prefix
   service = "function"
 }
