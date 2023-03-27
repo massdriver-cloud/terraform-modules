d1dc2cc6874870d86e3401bd594e3d8ce0883e39
Christopher Hill <chris@massdriver.cloud>
2023-03-27 14:52:47 -0600

    Don't output the entire application block in helm module (#159)

--- massdriver-application-helm/outputs.tf
+++ massdriver-application-helm/outputs.tf
@@ -1,4 +1,4 @@
-output "application" {
-  description = "The massdriver-application specification."
-  value       = module.application
+output "identity" {
+  description = "The massdriver-application IAM entity id (AWS Role ARN, GCP Service Account email, Azure Principal ID)."
+  value       = module.application.id
 }
