0e573c3e1c840a50205aaa5fc02f69e6664678d6
Michael Lacore <michael@massdriver.cloud>
2023-03-30 14:03:58 -0600

    Fixing misnamed output (#160)

--- azure/storage-account/outputs.tf
+++ azure/storage-account/outputs.tf
@@ -2,7 +2,7 @@ output "account_id" {
   value = azurerm_storage_account.main.id
 }
 
-output "primary_blod_endpoint" {
+output "primary_blob_endpoint" {
   value = azurerm_storage_account.main.primary_blob_endpoint
 }
 
