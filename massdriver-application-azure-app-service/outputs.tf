e90132757ccf079a76f0147f4ede0acbcbbe1284
Michael Lacore <michael@massdriver.cloud>
2023-04-17 15:36:36 -0600

    Adding outputs for artifacts in templates (#166)

--- massdriver-application-azure-app-service/outputs.tf
+++ massdriver-application-azure-app-service/outputs.tf
@@ -2,3 +2,13 @@ output "id" {
   description = "Cloud ID for application IAM (AWS Role, GCP Service Account, Azure Managed Identity ID, etc)"
   value       = module.application.id
 }
+
+output "resource_id" {
+  description = "Cloud Resource ID for the resource"
+  value       = azurerm_linux_web_app.main.id
+}
+
+output "hostname" {
+  description = "Default hostname for the application runtime"
+  value       = azurerm_linux_web_app.main.default_hostname
+}
