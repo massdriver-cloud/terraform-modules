abd42adcaa876dd310ced9c14cae2199c3ab2866
Michael Lacore <michael@massdriver.cloud>
2023-04-20 21:58:56 -0600

    Add hostname output (#169)

--- massdriver-application-gcp-cloud-run/outputs.tf
+++ massdriver-application-gcp-cloud-run/outputs.tf
@@ -2,3 +2,7 @@ output "id" {
   description = "The massdriver-application specification."
   value       = module.application.id
 }
+
+output "hostname" {
+  value = google_cloud_run_service.main.status.0.url
+}
