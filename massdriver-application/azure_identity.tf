ed57eecd759e68fcbc6fdc4125e5316bf42abd98
chrisghill <chris@massdriver.cloud>
2023-05-06 18:33:36 -0600

    small tweak

--- massdriver-application/azure_identity.tf
+++ massdriver-application/azure_identity.tf
@@ -5,7 +5,7 @@ locals {
     kubernetes = local.is_kubernetes ? {
       namespace            = var.kubernetes.namespace
       service_account_name = var.name
-      oidc_issuer_url      = try(var.kubernetes.cluster_artifact.data.infrastructure.oidc_issuer_url, "")
+      oidc_issuer_url      = var.kubernetes.cluster_artifact.data.infrastructure.oidc_issuer_url
     } : null
   }
 }
