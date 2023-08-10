3ba41fedf423d58fa05c0acbba9fcee2f144f753
Christopher Hill <chris@massdriver.cloud>
2023-08-10 00:10:23 -0600

    bump internal module refs (#182)

--- massdriver/k8s-application-alarms/main.tf
+++ massdriver/k8s-application-alarms/main.tf
@@ -1,7 +1,7 @@
 // POD ALARMS
 module "pod_crash_looping" {
   count                 = var.pod_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "Pods Crash Looping"
   prometheus_alert_name = "KubePodCrashLooping"
@@ -9,7 +9,7 @@ module "pod_crash_looping" {
 
 module "pod_not_ready" {
   count                 = var.pod_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "Pods Ready"
   prometheus_alert_name = "KubePodNotReady"
@@ -19,7 +19,7 @@ module "pod_not_ready" {
 // DEPLOYMENT ALARMS
 module "deployment_generation_mismatch" {
   count                 = var.deployment_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "Deployment Rollout Successful"
   prometheus_alert_name = "KubeDeploymentGenerationMismatch"
@@ -28,7 +28,7 @@ module "deployment_generation_mismatch" {
 // STATEFULSET ALARMS
 module "statefulset_generation_mismatch" {
   count                 = var.statefulset_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "StatefulSet Rollout Successful"
   prometheus_alert_name = "KubeStatefulSetGenerationMismatch"
@@ -37,7 +37,7 @@ module "statefulset_generation_mismatch" {
 // DAEMONSET ALARMS
 module "daemonset_generation_mismatch" {
   count                 = var.daemonset_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "DaemonSet Rollout Successful"
   prometheus_alert_name = "KubeDaemonSetRolloutStuck"
@@ -46,7 +46,7 @@ module "daemonset_generation_mismatch" {
 // JOB ALARMS
 module "job_failed" {
   count                 = var.job_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "Jobs Succeeded"
   prometheus_alert_name = "KubeJobFailed"
@@ -55,7 +55,7 @@ module "job_failed" {
 // HPA ALARMS
 module "hpa_maxed_out" {
   count                 = var.hpa_alarms ? 1 : 0
-  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=2ba8cd9b49c081c78f659f8c19b9026d73468abf"
+  source                = "github.com/massdriver-cloud/terraform-modules//k8s/prometheus-alarm?ref=f8a6406"
   md_metadata           = var.md_metadata
   display_name          = "Horizontal Pod Autoscaler Headroom"
   prometheus_alert_name = "KubeHpaMaxedOut"
