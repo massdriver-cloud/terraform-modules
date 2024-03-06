4ad42386f03204371586c9d3806fd74854678a05
mclacore <michael@massdriver.cloud>
2024-03-06 14:30:47 -0800

    Workload Identity for certmanager

--- k8s-cert-manager-azure/main.tf
+++ k8s-cert-manager-azure/main.tf
@@ -1,8 +1,17 @@
 module "cert-manager" {
-  source                 = "../k8s-cert-manager"
-  md_metadata            = var.md_metadata
-  kubernetes_cluster     = var.kubernetes_cluster
-  release                = var.release
-  namespace              = var.namespace
-  helm_additional_values = {}
+  source             = "../k8s-cert-manager"
+  md_metadata        = var.md_metadata
+  kubernetes_cluster = var.kubernetes_cluster
+  release            = var.release
+  namespace          = var.namespace
+  helm_additional_values = {
+    podLabels = {
+      "azure.workload.identity/use" = "true"
+    }
+    serviceAccount = {
+      labels = {
+        "azure.workload.identity/use" = "true"
+      }
+    }
+  }
 }
