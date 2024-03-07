a5925e73c9b6ae9466a5fe7fdda2dde3a7fe8da4
Michael Lacore <michael@massdriver.cloud>
2024-03-07 09:43:17 -0800

    Workload Identity support for AKS External DNS (#203)
    * Workload Identity support for AKS External DNS
    
    * Workload identity for external DNS working
    
    * Removing commented out service principal
    
    * Adding tags to managed identity

--- k8s-external-dns-azure/iam.tf
+++ k8s-external-dns-azure/iam.tf
@@ -1,30 +1,37 @@
 locals {
   # This needs to match the SA name in the submodule
   service_account_name = var.release == "external-dns" ? "external-dns" : "${var.release}-external-dns"
-  domain_filters       = join(",", tolist(var.azure_dns_zones.dns_zones))
+  # aks_oidc_short       = replace(var.kubernetes_cluster.data.infrastructure.oidc_issuer_url, "https://", "")
+  domain_filters = join(",", tolist(var.azure_dns_zones.dns_zones))
 }
 
 data "azurerm_client_config" "current" {
 }
 
-## Azure AD application that represents the app
-resource "azuread_application" "external_dns" {
-  display_name = "${var.md_metadata.name_prefix}-externaldns"
+data "azurerm_resource_group" "external_dns" {
+  name = var.azure_dns_zones.resource_group
 }
 
-## Azure AD app is required to create the service principal
-resource "azuread_service_principal" "external_dns" {
-  application_id = azuread_application.external_dns.application_id
+resource "azurerm_user_assigned_identity" "external_dns" {
+  location            = data.azurerm_resource_group.external_dns.location
+  name                = "${var.md_metadata.name_prefix}-externaldns"
+  resource_group_name = data.azurerm_resource_group.external_dns.name
+  tags                = var.md_metadata.default_tags
 }
 
-resource "azuread_service_principal_password" "external_dns" {
-  service_principal_id = azuread_service_principal.external_dns.id
+resource "azurerm_federated_identity_credential" "external_dns" {
+  name                = "${var.md_metadata.name_prefix}-externaldns-fc"
+  resource_group_name = data.azurerm_resource_group.external_dns.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = var.kubernetes_cluster.data.infrastructure.oidc_issuer_url
+  parent_id           = azurerm_user_assigned_identity.external_dns.id
+  subject             = "system:serviceaccount:${var.namespace}:${local.service_account_name}"
 }
 
 resource "azurerm_role_assignment" "external_dns" {
   for_each             = toset(["Reader", "DNS Zone Contributor"])
   scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  principal_id         = azuread_service_principal.external_dns.id
+  principal_id         = azurerm_user_assigned_identity.external_dns.principal_id
   role_definition_name = each.key
 }
 
@@ -36,11 +43,10 @@ resource "kubernetes_secret" "external_dns" {
   }
   data = {
     "azure.json" = jsonencode({
-      tenantId        = data.azurerm_client_config.current.tenant_id
-      subscriptionId  = data.azurerm_client_config.current.subscription_id
-      resourceGroup   = var.azure_dns_zones.resource_group
-      aadClientId     = azuread_service_principal.external_dns.application_id
-      aadClientSecret = azuread_service_principal_password.external_dns.value
+      tenantId                     = data.azurerm_client_config.current.tenant_id
+      subscriptionId               = data.azurerm_client_config.current.subscription_id
+      resourceGroup                = data.azurerm_resource_group.external_dns.name
+      useWorkloadIdentityExtension = true
     })
   }
 }
