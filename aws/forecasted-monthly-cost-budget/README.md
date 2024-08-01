10c52e868f30e3f8046f138ff621171607cad101
Cory O'Daniel <cory@coryodaniel.com>
2024-08-01 13:12:00 -0700

    Experimenting with schemas tied to reusable modules

---
+++ aws/forecasted-monthly-cost-budget/README.md
@@ -0,0 +1,3 @@
+# AWS Budget Management Terraform Module
+
+This Terraform module provides a simple and flexible way to manage AWS budgets, allowing users to monitor and control their AWS spending. It creates a budget that tracks the cost of your AWS resources and notifies you when your spending exceeds predefined thresholds.
---
+++ aws/forecasted-monthly-cost-budget/main.tf
@@ -0,0 +1,51 @@
+variable "name_suffix" {
+  type    = string
+  default = null
+}
+
+variable "limit_amount" {
+  default = 500
+}
+
+variable "subscriber_email_addresses" {
+  default = []
+}
+
+variable "subscriber_sns_topic_arns" {
+  default = []
+}
+
+variable "md_metadata" {
+  type = any
+}
+
+locals {
+  name              = var.name_suffix != null ? var.md_metadata.name_prefix + "-" + var.name_suffix : var.md_metadata.name_prefix
+  package_tag_value = var.md_metadata.default_tags["md-package"]
+}
+
+resource "aws_budgets_budget" "main" {
+  name         = local.name
+  budget_type  = "COST"
+  limit_amount = var.limit_amount
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  cost_filter {
+    name = "TagKeyValue"
+    values = [
+      "md-package${"$"}${local.package_tag_value}"
+    ]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = var.subscriber_email_addresses
+    subscriber_sns_topic_arns  = var.subscriber_sns_topic_arns
+  }
+
+  tags = var.md_metadata.default_tags
+}
